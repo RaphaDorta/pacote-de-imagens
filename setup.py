f5ff1ed7e3dc0b0b680d43d3689547cca502e558
Raphael Dorta <raphael_dorta@hotmail.com>
2024-09-11 14:04:11 -0300

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pacote_de_imagens',  # Nome do pacote deve estar em minúsculas e usar underscores
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Adicione suas dependências aqui, se houver
+    ],
+)
